938a04ac0dc95bb9a9a825a59e139b030d0dbe08
inza-tech <inzop7043@gmail.com>
2025-10-16 17:58:07 +0000

    OK

--- next.config.mjs
+++ next.config.mjs
@@ -9,13 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config, { dev }) => {
-    // Désactive le cache Webpack uniquement en développement
-    if (dev) {
-      config.cache = false;
-    }
-    return config;
-  },
-};
+}
 
-export default nextConfig;
+export default nextConfig
