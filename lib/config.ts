60d8259212709346f213c040b124ac2f5242f39f
TymyProx <mohamed.tou@proxyma-technologies.net>
2025-11-18 12:16:12 +0000

    OK

--- lib/config.ts
+++ lib/config.ts
@@ -14,5 +14,5 @@ export const config = {
   TENANT_ID: cleanEnvValue(process.env.NEXT_PUBLIC_TENANT_ID, "aa1287f6-06af-45b7-a905-8c57363565c2"),
   
   // URL du portail e-banking client (pour les liens d'invitation)
-  EBANKING_URL: cleanEnvValue(process.env.NEXT_PUBLIC_EBANKING_URL, "https://eportal.bng.astratechnologies.app/"),
+  EBANKING_URL: cleanEnvValue(process.env.NEXT_PUBLIC_EBANKING_URL, "https://35.184.98.9:4000"),
 }
